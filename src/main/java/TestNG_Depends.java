d879085418b21b01bda400cfcd17a66938c3bda4
Reena <Reenapatel.qa@gmail.com>
2022-09-25 21:00:22 -0400

    Committed TestNG Depends on method

---
+++ src/main/java/TestNG_Depends.java
@@ -0,0 +1,49 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestNG_Depends {
+
+	
+	WebDriver driver;
+
+	@BeforeMethod
+	public void initialization() {
+		System.setProperty("webdriver.chrome.driver", "C:/Users/patel/Webdriver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+
+		driver.get("https://www.saucedemo.com/");
+
+	}
+
+	@Test
+	public void login() {
+		driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce");
+		driver.findElement(By.xpath("//input[@id='login-button']")).click();
+	}
+
+	@Test(dependsOnMethods = "login", priority = 1)
+	public void titleTest() {
+		String title = driver.getTitle();
+		System.out.println(title);
+	}
+
+	@Test(dependsOnMethods = "login", priority = 1)
+	public void logoTest() {
+		boolean logo = driver.findElement(By.xpath("//div[@class='login_logo']")).isDisplayed();
+	}
+
+	@AfterMethod
+	public void teardown() {
+		driver.quit();
+	}
+}
